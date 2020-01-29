320ae2ca67ef8c1205f6e16f55c164deb078e10b
AdamSiekierski <adam.siekiera@outlook.com>
2020-01-29 10:42:08 +0100

    Initial commit

---
+++ predict.py
@@ -0,0 +1,35 @@
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+import pathlib
+
+model = keras.models.load_model('fruits_model.h5')
+
+print('Loading dataset...')
+data_dir = pathlib.Path('./fruits_dataset_test')
+class_names = np.array([ item.name for item in data_dir.glob('*') if item.name != '.DS_Store' ])
+image_count = len(list(data_dir.glob('*/*.jpg')))
+
+image_generator = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+test_data_gen = image_generator.flow_from_directory(directory=str(data_dir),
+                                                     shuffle=True,
+                                                     target_size=(224, 224),
+                                                     batch_size=25,
+                                                     classes = list(class_names))
+
+test_images, test_labels = next(test_data_gen)
+
+predictions = model.predict(test_images)
+
+plt.figure(figsize=(10, 10))
+for i in range(len(predictions)):
+  item_title = class_names[np.argmax(predictions[i])]
+  plt.subplot(5, 5, i+1)
+  plt_title = plt.title(item_title)
+  plt.imshow(test_images[i])
+  plt.axis('off')
+  if (np.argmax(predictions[i]) != np.argmax(test_labels[i])):
+    plt_title.set_color('red')
+
+plt.show()
---
+++ train.py
@@ -0,0 +1,44 @@
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pathlib
+
+# Prepare the dataset
+
+print('Loading dataset...')
+data_dir = pathlib.Path('./fruits_dataset_train')
+class_names = np.array([ item.name for item in data_dir.glob('*') if item.name != '.DS_Store' ])
+print(class_names)
+image_count = len(list(data_dir.glob('*/*.jpg')))
+
+image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+train_data_gen = image_generator.flow_from_directory(directory=str(data_dir),
+                                                     shuffle=True,
+                                                     batch_size=512,
+                                                     target_size=(224, 224),
+                                                     classes = list(class_names))
+
+train_images, train_labels = next(train_data_gen)
+
+# Create the model
+
+model = keras.Sequential([
+  keras.layers.Conv2D(32, 3, activation="relu", input_shape=(224, 224, 3)),
+  keras.layers.MaxPool2D(2),
+  keras.layers.Conv2D(32, 3, activation="relu"),
+  keras.layers.MaxPool2D(2),
+  keras.layers.Flatten(),
+  keras.layers.Dense(512, activation="relu"),
+  keras.layers.Dropout(0.5),
+  keras.layers.Dense(2, activation="softmax")
+])
+
+model.compile(optimizer='adam',
+              loss="categorical_crossentropy",
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+model.save('fruits_model.h5')
